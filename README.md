e538a22c5385ca9b8da2d43b3d70449f76114d5c
ScWen7 <xxh783965@gmail.com>
2018-07-18 15:50:55 +0800

    update README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 该项目是对动画进行的学习练习
 - Loading动画
 
-![image](https://upload-images.jianshu.io/upload_images/6113442-5c66b52a9379bb21.gif?imageMogr2/auto-orient/strip)
+![loading.gif](https://upload-images.jianshu.io/upload_images/6113442-fb58bb2734cc5580.gif?imageMogr2/auto-orient/strip)
 
 实现过程分析：[属性动画实现Loading动画](https://www.jianshu.com/p/1ac56c38d344)
 - ScrollView 滑动动画
--- app/src/main/java/scwen/com/animationtest/MainActivity.java
+++ app/src/main/java/scwen/com/animationtest/MainActivity.java
@@ -10,17 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        final LoadingView load = findViewById(R.id.load);
-//
-//        load.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                load.loadEnd();
-//            }
-//        },5000);
+        final LoadingView load = findViewById(R.id.load);
+
+        load.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                load.loadEnd();
+            }
+        },1500);
     }
 
-    public void start(View view) {
-        AnimFrameworkActivity.start(this);
-    }
+//    public void start(View view) {
+//        AnimFrameworkActivity.start(this);
+//    }
 }
